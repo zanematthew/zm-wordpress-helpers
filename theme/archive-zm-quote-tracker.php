059cee0c153dc41addb82a7cfbf29fb255f68b5b
Zane M. Kolnik <zanematthew@gmail.com>
2012-01-01 21:46:40 -0500

    Sidebar
    The sidebar is now its own file.
    
    Signed-off-by: Zane M. Kolnik <zanematthew@gmail.com>

--- theme/archive-zm-quote-tracker.php
+++ theme/archive-zm-quote-tracker.php
@@ -23,59 +23,14 @@ $cpt_obj = get_post_types( array( 'name' => $post_type), 'objects' );
 zm_cpt_json_feed( $post_type='zm-quote-tracker', $taxonomies=$cpt_obj[ $post_type ]->taxonomies );
 ?>
 <div class="container-zm-quote-tracker">
-    <div class="taxonomy-zm-quote-tracker">
-        <!-- Start 'sidebar' -->
-        <div class="sidebar-zm-quote-tracker">
-            <ul>
-                <li>Admin Menu</li>
-                <li><?php do_action('create_quote'); ?></li>
-                <?php if ( current_user_can( 'administrator' ) ) : ?>
-                    <li><a href="<?php bloginfo('wpurl');?>/wp-admin" title="Click to go to WordPress admin">WordPress Admin</a></li>
-                <?php endif; ?>                                                 
-                <li>&nbsp;</li>
-                <li><a href="<?php echo wp_logout_url( 'http://' . $_SERVER['HTTP_HOST'] . '/quotes' ); ?>" title="Click here to Log out">Logout</a></li>
-            </ul>
-            <span class="m-dash">&mdash;</span>                             
-<div id="search_navigation" class="search-navigation-container">
-    <em>Browse by</em> 
-    <a href="#" class="current">Basic</a><span class="bar">|</span><a href="#" class="inative">Advanced</a>
-</div>
-<div id="basic_search" class="search-by">
-    <?php zm_base_list_terms( array('taxonomy' => 'book', 'extra_class' => 'my-twipsy', 'post_type' => $post_type ) ); ?>            
-    <span class="m-dash">&mdash;</span>
-    <?php zm_base_list_terms( array('taxonomy' => 'movie', 'extra_class' => 'my-twipsy', 'post_type' => $post_type ) ); ?>            
-    <span class="m-dash">&mdash;</span>
-    <?php zm_base_list_terms( array('taxonomy' => 'song', 'extra_class' => 'my-twipsy', 'post_type' => $post_type ) ); ?>            
-    <span class="m-dash">&mdash;</span>
-    <?php zm_base_list_terms( array('taxonomy' => 'people', 'extra_class' => 'my-twipsy', 'post_type' => $post_type ) ); ?>            
-    <span class="m-dash">&mdash;</span>
-    <?php zm_base_list_terms( array('taxonomy' => 'zm-quote-tag', 'label' => 'Quote Tag', 'extra_class' => 'my-twipsy', 'post_type' => $post_type ) ); ?>            
-</div>
-<div id="advanced_search" class="search-by" style="display: none;">
-    <div class="zm-default-form-container">
-        <form action="javascript://" id="filter_task_form">        
-            <div class="form-wrapper">
-                <input type="hidden" value="task" name="post_type" />
-                <?php zm_base_build_input( array( 'taxonomy' => 'book', 'prepend' => 'book-', 'type' => 'checkbox' ) ); ?>                
-                <span class="m-dash">&mdash;</span> 
-                <?php zm_base_build_input( array( 'taxonomy' => 'movie', 'prepend' => 'movie-', 'type' => 'checkbox' ) ); ?>                
-                <span class="m-dash">&mdash;</span> 
-                <?php zm_base_build_input( array( 'taxonomy' => 'song', 'prepend' => 'song-', 'type' => 'checkbox' ) ); ?>                
-                <span class="m-dash">&mdash;</span> 
-                <?php zm_base_build_input( array( 'taxonomy' => 'people', 'prepend' => 'people-', 'type' => 'checkbox' ) ); ?>                
-                <span class="m-dash">&mdash;</span> 
-                <?php zm_base_build_input( array( 'taxonomy' => 'zm-quote-tag', 'prepend' => 'zm-quote-tag-', 'type' => 'checkbox', 'label' => 'Quote Tag' ) ); ?>                
-            </div>
-        </form>
-    </div>
-</div>            
-        </div>
-        <!-- End 'sidebar' -->
+    <div class="taxonomy-zm-quote-tracker">        
+
+        <?php load_template( plugin_dir_path( __FILE__ ) . 'sidebar-zm-quote-tracker.php'); ?>        
 
         <div class="main-zm-quote-tracker">        	                    
             <div id="filter_control_target" style="display: none;"></div>
             <div id="zm_ajax_the_loop">
-            <div class="hentry">                
+            <div class="hentry hentry-first">                
                 <h1>Quotes</h1>
                 <span class="m-dash">&mdash;</span>                 
                 <p>Archived collection of quotes.</p>
--- theme/taxonomy-zm-quote-tracker.php
+++ theme/taxonomy-zm-quote-tracker.php
@@ -24,55 +24,12 @@ zm_cpt_json_feed( $post_type='zm-quote-tracker', $taxonomies=$cpt_obj[ $post_typ
 ?>
 <div class="container-zm-quote-tracker">
     <div class="taxonomy-zm-quote-tracker">
-        <div class="sidebar-zm-quote-tracker">
-            <ul>
-                <li>Admin Menu</li>
-                <li><?php do_action('create_quote'); ?></li>
-                <?php if ( current_user_can( 'administrator' ) ) : ?>
-                    <li><a href="<?php bloginfo('wpurl');?>/wp-admin" title="Click to go to WordPress admin">WordPress Admin</a></li>
-                <?php endif; ?>                                                 
-				<span class="m-dash">&mdash;</span>    
-                <li><a href="<?php echo wp_logout_url( 'http://' . $_SERVER['HTTP_HOST'] . '/quotes' ); ?>" title="Click here to Log out">Logout</a></li>
-            </ul>
-
-<div id="search_navigation" class="search-navigation-container">
-    <em>Browse by</em> 
-    <a href="#" class="current">Basic</a><span class="bar">|</span><a href="#" class="inative">Advanced</a>
-</div>
-<div id="basic_search" class="search-by">
-    <?php zm_base_list_terms( array('taxonomy' => 'book', 'extra_class' => 'my-twipsy', 'post_type' => $post_type ) ); ?>            
-    <span class="m-dash">&mdash;</span>
-    <?php zm_base_list_terms( array('taxonomy' => 'movie', 'extra_class' => 'my-twipsy', 'post_type' => $post_type ) ); ?>            
-    <span class="m-dash">&mdash;</span>
-    <?php zm_base_list_terms( array('taxonomy' => 'song', 'extra_class' => 'my-twipsy', 'post_type' => $post_type ) ); ?>            
-    <span class="m-dash">&mdash;</span>
-    <?php zm_base_list_terms( array('taxonomy' => 'people', 'extra_class' => 'my-twipsy', 'post_type' => $post_type ) ); ?>            
-    <span class="m-dash">&mdash;</span>
-    <?php zm_base_list_terms( array('taxonomy' => 'zm-quote-tag', 'label' => 'Quote Tag', 'extra_class' => 'my-twipsy', 'post_type' => $post_type ) ); ?>            
-</div>
-<div id="advanced_search" class="search-by" style="display: none;">
-    <div class="zm-default-form-container">
-        <form action="javascript://" id="filter_task_form">        
-            <div class="form-wrapper">
-                <input type="hidden" value="task" name="post_type" />
-                <?php zm_base_build_input( array( 'taxonomy' => 'book', 'prepend' => 'book-', 'type' => 'checkbox' ) ); ?>                
-                <span class="m-dash">&mdash;</span> 
-                <?php zm_base_build_input( array( 'taxonomy' => 'movie', 'prepend' => 'movie-', 'type' => 'checkbox' ) ); ?>                
-                <span class="m-dash">&mdash;</span> 
-                <?php zm_base_build_input( array( 'taxonomy' => 'song', 'prepend' => 'song-', 'type' => 'checkbox' ) ); ?>                
-                <span class="m-dash">&mdash;</span> 
-                <?php zm_base_build_input( array( 'taxonomy' => 'people', 'prepend' => 'people-', 'type' => 'checkbox' ) ); ?>                
-                <span class="m-dash">&mdash;</span> 
-                <?php zm_base_build_input( array( 'taxonomy' => 'zm-quote-tag', 'prepend' => 'zm-quote-tag-', 'type' => 'checkbox', 'label' => 'Quote Tag' ) ); ?>                
-            </div>
-        </form>
-    </div>
-</div>
-        </div>
-        <!-- End 'sidebar' -->
+       
+       <?php load_template( plugin_dir_path( __FILE__ ) . 'sidebar-zm-quote-tracker.php'); ?>
 
         <div class="main-zm-quote-tracker">        	
-        	<div class="hentry">
+            <div id="filter_control_target" style="display: none;"></div>
+        	<div class="hentry hentry-first">
             	<?php $taxonomy = get_term( $wp_query->queried_object->term_id, $wp_query->query_vars['taxonomy'] ); ?>            	
 				<h1><?php print $taxonomy->name; ?></h1>
             	<span class="m-dash">&mdash;</span>    				
