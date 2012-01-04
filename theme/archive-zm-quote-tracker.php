c56bec8d4f95bc7417aa84bbca43e427f352eeb5
Zane M. Kolnik <zanematthew@gmail.com>
2012-01-04 08:16:15 -0500

    Cleaner Templates
    An attempt at condensing templates, using the-loop.php for now.
    
    Signed-off-by: Zane M. Kolnik <zanematthew@gmail.com>

--- theme/archive-zm-quote-tracker.php
+++ theme/archive-zm-quote-tracker.php
@@ -24,35 +24,13 @@ zm_cpt_json_feed( $post_type='zm-quote-tracker', $taxonomies=$cpt_obj[ $post_typ
 ?>
 <div class="container-zm-quote-tracker">
     <div class="taxonomy-zm-quote-tracker">        
-
         <?php load_template( plugin_dir_path( __FILE__ ) . 'sidebar-zm-quote-tracker.php'); ?>        
-
         <div class="main-zm-quote-tracker">        	                    
             <div id="filter_control_target" style="display: none;"></div>
             <div id="zm_ajax_the_loop">
-            <div class="hentry hentry-first">                
-                <h1>Quotes</h1>
-                <span class="m-dash">&mdash;</span>                 
-                <p>Archived collection of quotes.</p>
-            </div>            
-            <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-                <div <?php post_class('result')?>>
-                    <h2 class="title post-title" data-post_id="<?php echo $post->ID; ?>"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <div class="utility-zm-quote-tracker">
-                    	<div class="tag-container">
-    						<?php print zm_base_get_the_term_list( array( 'post_id' => $id , 'post_type' => $post_type, 'taxonomy' => 'zm-quote-tag', 'link' => 'anchor') ); ?>
-    					</div>
-                    </div>
-                    <!-- End 'utility' -->
-                </div>
-                <!-- End 'post_class' -->
-            <?php endwhile; ?>
+                <?php load_template( plugin_dir_path( __FILE__ ) . 'the-loop.php'); ?>
             </div>
         </div>
-        <!-- End 'main' -->
     </div>
-    <!-- End 'single' -->
 </div>
-<!-- End 'container' -->
-
 <?php get_footer(); ?>
